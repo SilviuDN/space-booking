81f71464dab328f4170e1cb1ba1442991a7740ce
Sgallego <sgallegogirona@gmail.com>
2021-07-19 23:24:02 +0200

    pro reviews

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ module.exports = app => {
     app.use('/api', require('./auth.routes.js'))
     app.use('/api/admin', require('./admin.routes'))
     app.use('/api/user', require('./user.routes'))
-    app.use('/api/reviews', require('./review.routes'))
+    app.use('/api/review', require('./review.routes'))
     app.use('/api/company', require('./company.routes'))
     app.use('/api/destination', require('./destination.routes'))
     app.use('/api/airport', require('./airport.routes'))
--- server/routes/review.routes.js
+++ server/routes/review.routes.js
@@ -6,18 +6,33 @@ const Destination = require('../models/Destination.model')
 const Review = require('../models/Review.model')
 
 // mimddleware solo admin
-router.get('/new/:id/:which', (req, res) => {
+router.post('/new/:id/:which', (req, res) => {
 
-    const {wich, id} = req.params
+    const {which, id} = req.params
 
-    Review.create(body)
+    Review.create(req.body)
         .then(response => {
 
+
+            // if (which === 'airport') {
+            //      Airport.findByIdAndUpdate(id, {$push:{reviews:response._id}})
+            //         .then(response  =>  res.send(response))
+            //         .catch(err => console.log(err))
+            // }
             
-            which === 'airport' ? Airport.findByIdAndUpdate(id, {$push:{reviews:response._id}}) :
-            which === 'company' ? Company.findByIdAndUpdate(id, {$push:{reviews:response._id}}) :
-            which === 'destination' ? Destination.findByIdAndUpdate(id, {$push:{reviews:response._id}}) :
-            res.status(500).json({ code: 500, message: 'Coleccion no encontrada no encontrada', err })
+            which === 'airport' ? Airport.findByIdAndUpdate(id, {$push:{reviews:response._id}})
+                .then(response  =>  res.json(response))
+                .catch(err => console.log(err)) :
+                
+                which === 'company' ? Company.findByIdAndUpdate(id, {$push:{reviews:response._id}})
+                    .then(response  =>  res.json(response))
+                    .catch(err => console.log(err)) :
+                    
+                    which === 'destination' ? Destination.findByIdAndUpdate(id, {$push:{reviews:response._id}})
+                        .then(response  =>  res.json(response))
+                        .catch(err => console.log(err)) :
+                        
+                        res.status(500).json({ code: 500, message: 'Coleccion no encontrada no encontrada', err })
             
         })
         .catch(err => console.log(err))
