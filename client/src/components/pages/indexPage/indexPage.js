5a10524305aa7f8f7900030ab8bb546237e937a5
amarinoglobal <81358493+amarinoglobal@users.noreply.github.com>
2021-07-24 18:29:16 +0200

    index full

--- client/src/components/pages/indexPage/indexPage.js
+++ client/src/components/pages/indexPage/indexPage.js
@@ -1,6 +1,10 @@
 // import { Link } from 'react-router-dom';
 import { Component } from 'react'
-
+import { Carousel } from "react-bootstrap"
+import c1 from './c1.jpg'
+import c2 from './c2.jpg'
+import c3 from './c3.jpg'
+import './IndexPage.css'
 
 class IndexPage extends Component {
 
@@ -18,7 +22,35 @@ class IndexPage extends Component {
         return (
 
             <>
-                <h1>hello</h1>
+
+                <Carousel fade controls={false} >
+                    <Carousel.Item >
+                        <img
+                            className="d-block  c "
+                            src={c1}
+                            alt="First slide"
+                        />
+
+                    </Carousel.Item >
+                    <Carousel.Item >
+                        <img
+                            className="d-block  c"
+                            src={c2}
+                            alt="Second slide"
+                        />
+
+
+                    </Carousel.Item>
+                    <Carousel.Item >
+                        <img
+                            className="d-block  c"
+                            src={c3}
+                            alt="Third slide"
+                        />
+
+                    </Carousel.Item>
+                </Carousel>
+
             </>
         )
     }
