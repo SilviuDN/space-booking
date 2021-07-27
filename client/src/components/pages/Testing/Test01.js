dc4775c0019975a69a79531ad56f8833c1c0bf72
Silviu Dilimot <silviudilimot@gmail.com>
2021-07-27 02:28:24 +0200

    LinesChart almost ready

--- client/src/components/pages/Testing/Test01.js
+++ client/src/components/pages/Testing/Test01.js
@@ -3,17 +3,21 @@ import RandomChart from "./RandomChart"
 import FlightsOccupationChart from "./FlightsOccupationChart"
 import SoldToCapacityComparation from "./SoldToCapacityComparation"
 import LinesChart from "./LinesChart"
+import CompaniesChart from "./CompaniesChart"
 
 const Test01 = () => {
     return (
         <>
             {/* <FlightsPerDestination /> */}
             {/* <RandomChart /> */}
+
             <FlightsOccupationChart howManyDays={1}/>
             <FlightsOccupationChart howManyDays={2}/>
             <FlightsOccupationChart howManyDays={3}/>
             <FlightsOccupationChart />
             <LinesChart/>
+
+            <CompaniesChart/>
 
         </>
     )
