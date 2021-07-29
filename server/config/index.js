afbdfb5e3be196c30e907e37b51acf3aba64ec50
SilviuDN <silviudilimot@gmail.com>
2021-07-29 12:34:56 +0200

    deploy

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+
 const logger = require("morgan");
 
 const cookieParser = require("cookie-parser");
@@ -15,5 +16,7 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  
+  app.use(express.static(path.join(__dirname, "..", "public")))
 
 };
